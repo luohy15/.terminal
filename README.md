d2fbb02597ab7d47301b8f827962227aef64317c
luohy15 <luohycs@gmail.com>
2019-09-19 20:28:03 +0800

    feat(ansible): setup by ansible

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # how to use
 1. clone && cd
-2. bash setup.sh
+2. ansible -i hosts main.yml
 3. logout & login
---
+++ roles/install/files/install-oh-my-zsh.sh
@@ -0,0 +1,1 @@
+yes n | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
